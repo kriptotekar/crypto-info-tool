1745975c75cdbff69fbd1048014e5b9bca49d7bb
Aldo Urem <aldooneplus@gmail.com>
2025-04-18 14:13:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 // index.js Crypto Price Fetcher Tool
-// Installation: npm install -g crypto-info-tool
 // This script fetches the current price of a specified cryptocurrency in a specified currency.
 // Usage: node index.js <cryptoId> <currency> eg node index.js bitcoin usd or node index.js ethereum eur
 // Improvement of usage: crypto <cryptoId> <currency> (after installing globally)
